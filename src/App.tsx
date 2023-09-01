b15fac6426005ae3ad961124727c639f97d98abf
Kevin Tavares <kvatavares30@icloud.com>
2023-09-01 16:26:19 -0300

    new-previc

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import Sigin from './Assets/sign-in.png';
 import './App.css';
 
+
+
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-dado'>
+      <h2>Cadastrar Mandato de Diretor Executivo</h2>
+      
+      <h6>DADOS PESSOAIS</h6>
+      
+      <div className='input-container'>
+        <label className='label-cpf'>
+          <span className='span-cpf'>CPF</span>
+          <br />
+          <input className='input-cpf' type='number' placeholder='Insira seu CPF' />
+        </label>
+
+        <label className='form-nome'>
+          <span className='nome-span'>Nome Completo</span>
+          <br />
+          <input className='input-nome' type='text' placeholder='Insira seu Nome' />
+        </label>
+
+       <img src={Sigin}/>
+      
+      
+      </div>
+
+      
+      <h6 className='dados-do-mandato'>Dados do Mandato</h6>
+      
+      <div className='container-mandato'>
+         <label className='label-mandato'>
+           <span>Entidade</span>
+           <br />
+           <input className='input-mandato' type='text'  />
+         </label>
+
+         <label className='form-mandato'>
+          <span className='mandato-span'>Nome do Cargo</span>
+          <br />
+          <input className='input-cargo' type='text' placeholder='insira seu nome' />
+         </label>
     </div>
+
+    <div className='outros'>
+       <label className='form-data'>
+         <span className='data-span'>Data Inicial</span>
+         <br />
+         <input className='input-data' type='text' placeholder='13/13/2013' />
+         </label>
+
+         <label className='form-final'>
+         <span className='mandato-span'>Data Final</span>
+         <br />
+         <input className='input-data-final' type='text' placeholder='03/03/2023' />
+         </label>
+
+      <div className='radio-group'>
+       <span>Titular</span>
+       <div className='radio-button'></div>
+       <span>Suplente</span>
+       <div className='radio-button'></div>
+       </div>
+    
+    </div>
+
+    <div className='segundo-menu'>
+      <a className='itens-menu2' href='/'>
+      <h3>Características</h3>
+      <h3>Funções Especiais</h3>
+      <h3>Habilitação</h3>
+      </a>
+      
+    </div>
+   
+    <hr />
+    <button className='btb-menu2'>+ incluir Caracteristica</button>
+
+    <div className='btn-footer'>
+      <button className='btn-cancelar'>Cancelar</button>
+      <button className='btn-salvar'>Salvar</button>
+
+    </div>
+         
+  </div>
   );
 }
-
-export default App;
+export default App
---
+++ src/TelaIncluirCaract.tsx
@@ -0,0 +1,14 @@
+
+
+const TelaIncluirCaract = () => {
+  return (
+    <div className='incluir-caract'>
+     <h3>Incluir Caracteristica</h3>
+     <p>Caracteristica</p>
+     <input/>
+     
+    </div>
+  )
+}
+
+export default TelaIncluirCaract;
